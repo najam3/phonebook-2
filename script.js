e4229f83073707d5abe3be1da24a630a44a1d8b2
najam3 <100590374+najam3@users.noreply.github.com>
2023-01-12 14:38:22 +0500

    Phonebook
    Functionality added for adding Contacts and search bar not working properly, but functionality is added.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(function() {
     $(".tabs").click(function(e){
      if(e.target.id == "contacts"){
-        $("#contact").show();
+        $(".contact").show();
         $(".added").hide();
         $(".header h1").text("All Contacts are displayed here")
      } else if(e.target.id == "recently-added"){
@@ -45,43 +45,74 @@ $(function closeImg() {
 //** When someone adds a number */
 
 $(function addNum() {
-$("#submit-btn").click(function(e){
-
+$("#form").submit(function(e){
    e.preventDefault();
-//   Dynamically creating an Element on click
-  const elem = document.createElement('div');
-  elem.setAttribute("class", "contact-new");
-  //Adding details of the contact
-  const span = document.createElement("span");
- elem.appendChild(span)
-  span.setAttribute("class", "contact-in");
+let name = $("#name").val();
+let number = $("#number").val();
+let description = $("#description").val();
 
 
-  
-//    // userObject.forEach(contact => {
-//    // $("span#name").html(`${contact.contactName}`);
-//    // $("span#number").html(`${contact.contactNum}`)  
-//    // $("dl#description").html(`${contact.contactDescription}`) 
-//    // });
-
-   const contactName = $("#name").val();
-   const contactNum = $("#number").val();
-   const contactDescription = $("#description").val();
- 
-  //Storing Data in localstorage
-  localStorage.setItem(  
-      {'name': contactName}.name, 
-      {'number': contactNum}.number,
-      {'description': contactDescription}.description 
-       );
-
-
-   span.innerHTML = "Contact Name: " + contactName + "<br>" +"<br>" + "Contact Number: " + contactNum + "<br>" + "<br>" + "Description: " + contactDescription;
-
-$(".contacts").append(elem);
-$(".modal").hide();
-  
+localStorage.setItem(name, JSON.stringify({number, description}));
+$("#form").hide();
+window.location.reload();
 })
 }) 
 
+window.addEventListener("load", () => {
 
+
+for(let key in localStorage){
+   let contacts = JSON.parse(localStorage.getItem(key));
+    $(".contact").append(
+`<li><b>Name:</b> &nbsp;   ${key}<br> <br> 
+<b> Number:</b> &nbsp;  ${contacts.number} <br> <br> <br>
+<b> Description:</b> &nbsp; <span> &#128509; ${contacts.description}</span></li> 
+`
+    )
+    const elem = document.querySelector(".contact")
+let count = elem.children;
+
+console.log(count.length);
+$(".count").html(`${count.length - 1} &#9743;`)
+}
+
+
+})
+
+
+$(function search(){
+   $("#contact-list").on("change", function() {
+      var inputValue = $(this).val();
+   
+      // Do something with the input value
+      for (let key in localStorage) {
+   let contacts = JSON.parse(localStorage.getItem(key));
+          if(key.toLowerCase() === inputValue){
+            $(".contact").html(
+               `<li><b>Name:</b> &nbsp;   ${key}<br> <br> 
+               <b> Number:</b> &nbsp;  ${contacts.number} <br> <br> <br>
+               <b> Description:</b> &nbsp; <span> &#128509; ${contacts.description}</span></li> 
+               `
+                   )
+         } else if(inputValue === "") {
+            window.location.reload();
+         }
+      }
+
+    });
+    
+})
+
+
+
+const elem = document.querySelector(".contact");
+
+let childElem = elem.children;
+ console.log(childElem.length);
+
+   for(let i = 0; i <= childElem.length; i++) 
+   {
+
+       console.log(childElem[i]);
+
+   }
